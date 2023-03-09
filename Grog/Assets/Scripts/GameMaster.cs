737e03bd8042e53515f4130ad402c590c000abd8
Lem Apperson <lemapp09@gmail.com>
2023-03-09 09:27:51 -0500

    Add Level UI Display
    Add Level UI Display

--- Grog/Assets/Scripts/GameMaster.cs
+++ Grog/Assets/Scripts/GameMaster.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class GameMaster : MonoBehaviour
 {
@@ -15,6 +15,12 @@ public class GameMaster : MonoBehaviour
 
     public static GameMaster Instance;
     private int _currentPlaneId = 0;
+    [SerializeField] private GameObject _UISample;
+    [SerializeField] private TextMeshProUGUI _headerText;
+    [SerializeField] private TextMeshProUGUI _modalText;
+    private int _level = 1;
+    private float _levelTimer;
+
 
     void Awake() {
         Instance = this; 
@@ -32,16 +38,31 @@ public class GameMaster : MonoBehaviour
         _audioSource.Stop();
     }
 
+    private void Update() {
+        _levelTimer += Time.deltaTime;
+    }
 
     public void PlaneCrash(int planeID)
-    {           
-        Debug.Log("Plane #" + planeID + " has crashed!");
+    {
         _currentPlaneId++;
         if (_currentPlaneId < _planes.Length) {
             _planes[_currentPlaneId].gameObject.SetActive(true);
         } else {
-            Debug.Log("Level I complete");
+            StartCoroutine(DisplayLevelWin(_level));
         }
     }
-
+    private IEnumerator DisplayLevelWin(int level)
+    {
+        _UISample.SetActive(true);
+        _headerText.text = "Level " + level;
+        _modalText.text = "Grog has won Level " + level +
+                          "\n It took " + TimeSpan.FromSeconds(_levelTimer).Minutes + 
+                          ":" + TimeSpan.FromSeconds(_levelTimer).Seconds;
+        yield return new WaitForSeconds(10f);
+        _UISample.SetActive(false);
+        _level++;
+        _currentPlaneId = 0;
+        _planes[_currentPlaneId].gameObject.SetActive(true);
+        _levelTimer = 0f;
+    }
 }
--- Grog/Assets/Scripts/PlaneCollision.cs
+++ Grog/Assets/Scripts/PlaneCollision.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using BezierSolution;
 using Unity.XR.CoreUtils;
@@ -24,8 +23,7 @@ public class PlaneCollision : MonoBehaviour
     // Sound Effect from
     // <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=14513">Pixabay</a>
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         _audioSource.Play();
         this.transform.localPosition = Vector3.zero;
         this.transform.localRotation = Quaternion.identity;
@@ -34,11 +32,9 @@ public class PlaneCollision : MonoBehaviour
             _flames.SetActive(false);
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         _audioSource.Stop();
-        if (this.GetComponent<Rigidbody>() != null)
-        {
+        if (this.GetComponent<Rigidbody>() != null) {
             Rigidbody rigidBody = this.GetComponent<Rigidbody>();
             _splineWalker.enabled = true;
             rigidBody.useGravity = false;
@@ -62,7 +58,7 @@ public class PlaneCollision : MonoBehaviour
                 _splineWalker.enabled = false;
                 rigidBody.useGravity = true;
                 rigidBody.isKinematic = false;
-                Vector3 vector = (this.transform.position - _xrOrigin.transform.position).normalized * 75f;
+                Vector3 vector = (this.transform.position - _xrOrigin.transform.position).normalized * 15f;
                 rigidBody.AddForce(vector.x, vector.y, vector.z, ForceMode.Impulse);
 
                 if (_crashClips.Length > 0)
@@ -85,19 +81,19 @@ public class PlaneCollision : MonoBehaviour
 
     private IEnumerator RemoveDownPlane()
     {
-        Debug.Log("Plane #" + _planeID + " has started countdown");
-        yield return new WaitForSeconds(5f);
-        Debug.Log("Plane #" + _planeID + " has ended countdown");
+        yield return new WaitForSeconds(15f);
         this.gameObject.SetActive(false);
     }
 
     private IEnumerator ResizePlane()
     {
-        float scale = 0.001f;
+        float scale = 0.0005f;
         while (scale < 0.2) {
             this.transform.localScale = Vector3.one * scale;
-            scale += 0.01f;
+            scale += 0.0005f;
             yield return new WaitForSeconds(0.01f);
         }
     }
+
+
 }
